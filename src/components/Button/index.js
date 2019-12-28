29ee417ac255ac91ea2146bd67b1b0a64d0604fc
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2019-12-27 22:16:10 -0200

    remove styles

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { Button } from 'antd';
 
-import './styles.scss';
-
 export default function ButtonComponent({ classe, label, onClick }) {
   return (
     <Button className={classe} onClick={onClick} type="submit">
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { Input } from 'antd';
 
-import './styles.scss';
-
 export default function InputComponent({ defaultValue, onBlur, placeholder, type }) {
   return (
     <Input defaultValue={defaultValue} onBlur={onBlur} placeholder={placeholder} type={type} />
